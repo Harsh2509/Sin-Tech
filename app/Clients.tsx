437a09a14ac45e382e35e44e564e16c5c07e8153
harsh2509 <harshchauhan6176@gmail.com>
2024-09-07 18:54:12 +0530

    update: Marquee update for our clients.

--- app/Clients.tsx
+++ app/Clients.tsx
@@ -4,40 +4,85 @@ import Marquee from "@/components/magicui/marquee";
 
 const reviews = [
   {
-    name: "Jack",
-    username: "@jack",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://avatar.vercel.sh/jack",
+    id: 1,
+    placeholder: "Punjab National Bank",
+    img: "/pnb.webp",
   },
   {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
+    id: 2,
+    placeholder: "ABInBev",
+    img: "/ABInBev.webp",
   },
   {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
+    id: 3,
+    placeholder: "Canara Bank",
+    img: "/CanaraBank.webp",
   },
   {
-    name: "Jane",
-    username: "@jane",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jane",
+    id: 4,
+    placeholder: "PoscoPosco India Processing Private Limited",
+    img: "/Posco.webp",
   },
   {
-    name: "Jenny",
-    username: "@jenny",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jenny",
+    id: 5,
+    placeholder: "Punjab & Sind Bank",
+    img: "/PNSBank.webp",
   },
   {
-    name: "James",
-    username: "@james",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/james",
+    id: 6,
+    placeholder: "Jamia Millia Islamia",
+    img: "/JamiaIslamia.webp",
+  },
+  {
+    id: 7,
+    placeholder: "Sharda University",
+    img: "/ShardaUniversity.webp",
+  },
+  {
+    id: 8,
+    placeholder:
+      "National Institute of Science Communication and Information Resources",
+    img: "/NISCAIR.webp",
+  },
+  {
+    id: 9,
+    placeholder: "National Science Foundation",
+    img: "/NationalScienceFoundation.webp",
+  },
+  {
+    id: 10,
+    placeholder: "Central Bank Of India",
+    img: "/CentralBankOfIndia.webp",
+  },
+  {
+    id: 11,
+    placeholder: "UCO Bank",
+    img: "/UCOBank.webp",
+  },
+  {
+    id: 12,
+    placeholder: "Salwan Education Trust",
+    img: "/SalwanPublicSchool.webp",
+  },
+  {
+    id: 13,
+    placeholder: "Mahagun India Pvt. Ltd.",
+    img: "/Mahagun.webp",
+  },
+  {
+    id: 14,
+    placeholder: "Mahagun India Pvt. Ltd.",
+    img: "/DhampurBioOrganics.webp",
+  },
+  {
+    id: 15,
+    placeholder: "Bhagwan Parshuram Institute of Technology",
+    img: "/BPIT.webp",
+  },
+  {
+    id: 16,
+    placeholder: "Deepalaya",
+    img: "/Deepalya.webp",
   },
 ];
 
@@ -46,35 +91,24 @@ const secondRow = reviews.slice(reviews.length / 2);
 
 const ReviewCard = ({
   img,
-  name,
-  username,
-  body,
+  placeholder,
+  id,
 }: {
   img: string;
-  name: string;
-  username: string;
-  body: string;
+  placeholder: string;
+  id: number;
 }) => {
   return (
     <figure
       className={cn(
-        "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
+        "relative w-full h-full cursor-pointer overflow-hidden rounded-xl border p",
         // light styles
         "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
         // dark styles
         "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
       )}
     >
-      <div className="flex flex-row items-center gap-2">
-        <img className="rounded-full" width="32" height="32" alt="" src={img} />
-        <div className="flex flex-col">
-          <figcaption className="text-sm font-medium dark:text-white">
-            {name}
-          </figcaption>
-          <p className="text-xs font-medium dark:text-white/40">{username}</p>
-        </div>
-      </div>
-      <blockquote className="mt-2 text-sm">{body}</blockquote>
+      <img src={img} alt={placeholder} className="w-20 h-20" />
     </figure>
   );
 };
@@ -82,14 +116,17 @@ const ReviewCard = ({
 export function MarqueeDemo() {
   return (
     <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+      <div className=" text-blue-700 text-4xl mb-4 capitalize font-bold">
+        OUR CLIENTS
+      </div>
       <Marquee pauseOnHover className="[--duration:20s]">
-        {firstRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
+        {firstRow.map((icon) => (
+          <ReviewCard key={icon.id} {...icon} />
         ))}
       </Marquee>
       <Marquee reverse pauseOnHover className="[--duration:20s]">
-        {secondRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
+        {secondRow.map((icon) => (
+          <ReviewCard key={icon.id} {...icon} />
         ))}
       </Marquee>
       <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
